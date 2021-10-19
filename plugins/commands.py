e0ff00ad7638212b492a19e6b5cdca00e6e10877
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-19 12:54:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -115,6 +115,7 @@ async def start(bot, cmd):
         
 @Client.on_callback_query()
 async def cb_data(bot, update):
+        clicked = query.from_user.id
     if update.data == "ownerheme":
         await query.answer(
             "᯽≫⋯⋯ʙᴏᴛ ᴏᴡɴᴇʀ ᴅᴇᴛᴀɪʟꜱ⋯⋯≪᯽",show_alert=True
