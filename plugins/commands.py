52cffe51e0d79d09f3c592369d3b4f241c4dc1c6
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-21 21:35:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -128,7 +128,7 @@ async def start(bot, cmd):
 @Client.on_callback_query()
 async def cb_data(bot, update):
     if update.data == "about":
-        await update.answer(ABOUT_TEXT.format(update.from_user.mention),show_alert=True,parse_mode=html)
+        await update.answer(ABOUT_TEXT.format(update.from_user.mention),show_alert=True,parse_mode="html")
     elif update.data == "close":   
         await update.message.delete()
 
