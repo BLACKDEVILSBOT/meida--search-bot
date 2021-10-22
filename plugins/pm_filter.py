8e7ff6702f86b28dbaca9309cd637e73eb29d288
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-22 11:44:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -95,14 +95,8 @@ async def group(client, message):
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"subinps#{file_id}")]
                     )
         else:
-            Send_message = await BOT.reply_photo(
-                chat_id=update.chat.id,
-                photo="https://telegra.ph/file/e489d2f7ca86a2abd7537.jpg",
-                caption="<b>Couldn't Find This Movie.Try Again..! ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>",
-                reply_to_message_id=update.message_id
-            ) 
-            await asyncio.sleep(5)
-            await Send_message.delete()
+            return
+        if not btn:
             return
 
         if len(btn) > 10: 
